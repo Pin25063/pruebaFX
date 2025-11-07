5c48a4e34d9cab1e72be359b2e1e2e9efb59fbfb
Pin25063 <pin25063@uvg.edu.gt>
2025-11-07 15:24:35 -0600

    Subiendo proyecto Maven JavaFX

---
+++ src/main/java/Main.java
@@ -0,0 +1,21 @@
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) {
+        Label label = new Label("JavaFX corriendo correctamente");
+
+        Scene scene = new Scene(label, 350, 100);
+        primaryStage.setTitle("Test JavaFX");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+}
